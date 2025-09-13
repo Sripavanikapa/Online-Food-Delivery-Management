31b8b0e2da304ce3822bd30376dde82debfa5c44
Taniya <2440011@cognizant.com>
2025-09-13 16:14:40 +0530

    pull

--- Domain/DTO/RestaurantCreateDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.DTO
-{
-    public class RestaurantCreateDto
-    {
-        public bool Status { get; set; }
-
-        public int UserId { get; set; } // references from user table
-    }
-}
