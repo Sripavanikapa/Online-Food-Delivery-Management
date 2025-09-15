f6a46c704be981dfd52c9eee2d235f3ecb1a370e
Pavani <2440012@cognizant.com>
2025-09-15 18:09:58 +0530

    deleted a file

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a974987ec1c4b265c7bcaf30d438d30ceabd72f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6756cf013a4a626440e0b0eb16cd64646e7e7e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
